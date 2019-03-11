89e4e87d5b88b52ea3b744e05ddece8bdee009db
zhaohuanhuan <1297972106@qq.com>
2019-03-11 19:34:19 +0800

    [ zhh2 part]

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -14,14 +14,22 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-    for (let i = 0; i < items.length; i++) {
-      if (this.items[i].sellIn >= 0) {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name == 'Aged Brie' && this.items[i].quality <= 50) {
+        this.items[i].quality++;
+        this.items[i].sellIn--;
+      } else if (this.items[i].name == 'Sulfuras') {
+        this.items[i].quality;
+        this.items[i].sellIn;
+      } else if (this.items[i].sellIn >= 0) {
         if (0 <= this.items[i].quality <= 50) {
           this.items[i].quality--;
           this.items[i].sellIn--
+        } else {
+          this.items[i].quality = 0;
         }
-      } else {
-        this.items[i].quality--;
+      } else if (this.items[i].sellIn < 0 && this.items[i].quality >= 2) {
+        this.items[i].quality-=2;
       } 
     }
   
