78dbe5cf65ad9b12cb9aed22a66c43c9100577c8
zhaohuanhuan <1297972106@qq.com>
2019-03-12 17:32:43 +0800

    clean code

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -79,46 +79,6 @@ class Shop {
       this.items[i].update();
     }
   }
-
-  updateAgedBrie(item) {
-    if (item.quality < 50) {
-      item.quality++;
-    } else {
-      item.quality = 50;
-    }
-    item.sellIn--;
-  }
-
-  updateSulfuras(item) {
-    item.quality = item.quality;
-    item.sellIn = item.sellIn;
-  }
-
-  updateBackstagePasses(item) {
-    if (item.sellIn > 10) {
-      item.quality++;
-    } else if (item.sellIn > 5 && item.sellIn <= 10) {
-      item.quality += 2;
-    } else if (item.sellIn > 0 && item.sellIn <= 5) {
-      item.quality += 3;
-    } else {
-      item.quality = 0;
-    }
-    item.sellIn--;
-  }
-  
-  updateNormal(item) {
-    if (item.sellIn >= 0) {
-      if (item.quality >= 1) {
-        item.quality--;
-      } else {
-        item.quality = 0;
-      }
-    } else if (item.sellIn < 0 && item.quality >= 2) {
-      item.quality -= 2;
-    } 
-    item.sellIn--;
-  }
 }
 
 
